9e17cd540ad1e93648e5fe50c458bca6eb89f976
이상운 <sangunlee6@gmail.com>
2023-03-22 13:45:26 +0900

    week03

--- week03/BOJ_2637.py
+++ week03/BOJ_2637.py
@@ -8,8 +8,6 @@ M = int(input())
 graph = [[] for _ in range(N+1)]
 indegree = [0]*(N+1)
 v = [[0]*(N+1) for _ in range(N+1)]
-# 부품 개수를 저장하는 리스트 v
-
 
 for _ in range(M):
     a,b,c = map(int, input().split())
@@ -21,14 +19,12 @@ q = deque()
 for i in range(N+1):
     if indegree[i] ==0:
         q.append(i)
-        v[i][i] = 1    # 기본 부품만 1 로 설정
+        v[i][i] = 1   
 
 while q:
     now = q.popleft()
-    for node in graph[now]:         # node 는 (다음 부품 번호, 그걸 만드는데 필요한 개수)
-        
+    for node in graph[now]:                 
         for i in range(1, N+1):
-            # 다음 부품을 만드는데 필요한 부품 계산, 현재 부품 만드는데 드는 개수에 다음 부품 만들때 필요한 부품 곱해줌
             next_part = node[0]
             count = node[1]
 
